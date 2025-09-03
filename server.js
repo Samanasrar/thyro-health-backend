74ff03f8e0cf91c0a659296b67f78cf36fb1beaf
Saman Asrar <your-github-samanasrar11@gmail.com>
2025-09-03 15:01:49 +0500

    Fix: use Railway dynamic port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import contactRoutes from "./routes/contact.js"; // ✅ correct path
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // allow frontend requests
